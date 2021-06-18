87799e1fc5713a08c24c502d87cf677cf3e145a7
patson <cardin.tiako@gmail.com>
2021-06-18 20:21:02 +0200

    initialisation de l'environnement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+//commence par le routeur puis le switch puis la route
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = (props) => {
+  return <div>Lets Buils it</div>;
+};
+
+export default Home;
